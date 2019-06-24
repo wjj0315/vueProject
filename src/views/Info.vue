b8505426d90bffb8a57058997b03857e5d06c82f
wangjunjie <wangjj@qdaoe.cn>
2019-06-24 13:34:43 +0800

    init

---
+++ src/views/Info.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    hello Info Component
+    <button type="button" @click="add()">添加</button>
+  </div>
+</template>
+<script>
+import store from "@/store";
+export default {
+  name: "Info",
+  store,
+  data() {
+    return {
+      // msg:store.state.count
+    };
+  },
+  mounted() {
+  },
+  methods: {
+    add() {
+      console.log("add event from info!");
+      // debugger
+      store.commit("increase");
+    },
+   
+  }
+};
+</script>
+<style scoped>
+</style>
+
+
