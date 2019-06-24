a02f47c50593abc82625a705407f64c9f92551a5
wangjunjie <wangjj@qdaoe.cn>
2019-06-24 10:57:57 +0800

    vue init

--- src/store.js
+++ src/store.js
@@ -1,16 +1,16 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue"
+import Vuex from "vuex"
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    count: 0
   },
   mutations: {
-
+    increase() {
+      this.state.count += 1;
+    }
   },
-  actions: {
-
-  }
-})
+  actions: {}
+});
